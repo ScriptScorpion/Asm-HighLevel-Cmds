afb0ca7dd2d42d36083c769b0fe233f4a1495fc4
ScriptScorpion <63147222+ScriptScorpion@users.noreply.github.com>
2025-08-28 12:12:48 +0300

    Clarity and for_loop

--- ownlangcmds.asm
+++ ownlangcmds.asm
@@ -1,9 +1,8 @@
 section .data ; variables
     msg: db ". ", 0
-    secret: db "Done!", 0
+    done: db "Done!", 10, 0
     newline: db 10
     counter: db 0 ; like 'int i = 0' in C/C++
-section .bss ; values in this section are usually buffer
 section .text
     global _start
 
@@ -18,10 +17,10 @@ printnl: ; function
 _start: ; where program starts
     jmp while_loop
 
-while_loop: ; for loop is same but instead of doing je, doing jl
-    cmp byte [counter], 5
-    je if
-    inc byte [counter]
+while_loop: 
+    cmp BYTE [counter], 5
+    je new_line
+    inc BYTE [counter]
     mov rax, 1
     mov rdi, 1
     lea rsi, msg 
@@ -29,23 +28,64 @@ while_loop: ; for loop is same but instead of doing je, doing jl
     syscall
     jmp while_loop
 
+new_line:
+    call printnl
+    jmp for_loop
+
+adder: 
+    add BYTE [counter], 1 
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, msg 
+    mov rdx, 2
+    syscall
+
+    jmp for_loop
+
+for_loop:
+    cmp BYTE [counter], 10
+    jl adder
+    jmp if
+
 if:
     call printnl
-    call printnl
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, done 
+    mov rdx, 6
+    syscall
 
-    cmp byte [counter], 0
+    cmp BYTE [counter], 0 ; just example
     je exit
     jmp else
 
 else:
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, secret
-    mov rdx, 18
-    syscall
     jmp exit
 
 exit:
     mov rax, 60
     xor rdi, rdi
     syscall
+
+; This code written in C++ and describes all assembly above
+; #include <iostream>
+; int main() {
+;     int i = 0;
+;     while (i != 5) {
+;	  ++i;
+;         printf(". ");
+;     }
+;     printf("\n");
+;     for (; i < 10; ++i) {
+;         printf(". ");
+;     }
+;     printf("\n");
+;     printf("Done!\n");
+;     if (i == 0) {
+;         return 0;
+;     }
+;     else {
+;         return 0;
+;     }
+;
+; }
